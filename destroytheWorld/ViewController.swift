24fc9ab3b820f78da6afbed9530d7fa303eb9245
Zdravko Dimchov <zdimchov@gmail.com>
2017-01-02 13:51:32 +0200

    hello world

---
+++ destroytheWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  destroytheWorld
+//
+//  Created by Zdravko Dimchov on 02/01/17.
+//  Copyright © 2017 Zdravko Dimchov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
